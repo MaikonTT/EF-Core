f232330a7568a4c752c9d86a802c0f05f305f1b7
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-05-02 00:56:39 -0300

    Alterado configuração de mapeamento de entidades

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,5 +1,4 @@
-﻿using CursoEFCore.Domain;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore.Data
 {
@@ -14,51 +13,7 @@ namespace CursoEFCore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(p =>
-            {
-                p.ToTable("Clientes");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Nome).HasColumnType("NVARCHAR(14)").IsRequired();
-                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
-                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
-                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
-
-                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
-            });
-
-            modelBuilder.Entity<Pedido>(p =>
-            {
-                p.ToTable("Pedidos");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.IniciadoEm).HasDefaultValue("GETDATE()").ValueGeneratedOnAdd();
-                p.Property(p => p.Status).HasConversion<string>();
-                p.Property(p => p.TipoFrete).HasConversion<int>();
-                p.Property(p => p.Observacao).HasColumnType("NVARCHAR(512)");
-
-                p.HasMany(p => p.Itens)
-                    .WithOne(p => p.Pedido)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<Produto>(p =>
-            {
-                p.ToTable("Produtos");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
-                p.Property(p => p.Descricao).HasColumnType("NVARCHAR(60)");
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.Tipo).HasConversion<string>();
-            });
-
-            modelBuilder.Entity<PedidoItem>(p =>
-            {
-                p.ToTable("PedidoItens");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired();
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.Desconto).IsRequired();
-            });
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
