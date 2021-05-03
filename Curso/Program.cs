99e194cd3050a4b87b56f529fba4e12de7b9d5b2
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-05-03 20:23:45 -0300

    Retirado comentários desnecessários

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -12,7 +12,7 @@ namespace CursoEFCore
     {
         public static void Main(string[] args)
         {
-            using var db = new Data.ApplicationContext();
+            using var db = new ApplicationContext();
             var existe = db.Database.GetPendingMigrations().Any();
 
             if (existe)
@@ -24,23 +24,19 @@ namespace CursoEFCore
                 Console.WriteLine("Não existe migrações pendentes!");
             }
 
-            //InserirDados();
-            //InserirDadosEmMassa();
-            //ConsultarDados();
-            //CadastrarPedido();
-            //ConsultarPedidoCarregamentoAdiantado();
-            //AtualizarDados();
+            InserirDados();
+            InserirDadosEmMassa();
+            ConsultarDados();
+            CadastrarPedido();
+            ConsultarPedidoCarregamentoAdiantado();
+            AtualizarDados();
             RemoverRegistro();
         }
 
         private static void RemoverRegistro()
         {
             using var db = new ApplicationContext();
-            //var cliente = db.Clientes.Find(3);
-            var cliente = new Cliente
-            {
-                Id = 4
-            };
+            var cliente = db.Clientes.Find(3);
 
             db.Clientes.Remove(cliente);
 
@@ -50,22 +46,11 @@ namespace CursoEFCore
         private static void AtualizarDados()
         {
             using var db = new ApplicationContext();
-            //var cliente = db.Clientes.Find(3);
+            var cliente = db.Clientes.FirstOrDefault();
 
-            var cliente = new Cliente
-            {
-                Id = 3
-            };
+            cliente.Nome = "Cliente Desconectado Passo 1";
+            cliente.Telefone = "11912345678";
 
-            var clienteDesconectado = new
-            {
-                Nome = "Cliente Desconectado Passo 3",
-                Telefone = "11912345678"
-            };
-
-            db.Attach(cliente);
-            db.Entry(cliente).CurrentValues.SetValues(clienteDesconectado);
-            //db.Clientes.Update(cliente);
             db.SaveChanges();
         }
 
@@ -115,7 +100,6 @@ namespace CursoEFCore
         private static void ConsultarDados()
         {
             using var db = new ApplicationContext();
-            //var consultaPorSintaxe = (from c in db.Clientes where c.Id > 0 select c).ToList();
             var consultaPorMetodo = db.Clientes
                 .Where(p => p.Id > 0)
                 .OrderBy(p => p.Id)
@@ -124,7 +108,6 @@ namespace CursoEFCore
             foreach (var cliente in consultaPorMetodo)
             {
                 Console.WriteLine($"Consultando cliente: {cliente.Id}");
-                //db.Clientes.Find(cliente.Id);
                 db.Clientes.FirstOrDefault(p => p.Id == cliente.Id);
             }
         }
@@ -189,7 +172,7 @@ namespace CursoEFCore
         };
 
             using var db = new ApplicationContext();
-            //db.AddRange(produto, cliente);
+            db.AddRange(produto, cliente);
 
             db.Clientes.AddRange(listaClientes);
 
