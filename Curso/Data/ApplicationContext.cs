e86a52e9a25842ffa5b2780de9f295cbfbf861ca
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-05-02 23:50:06 -0300

    Adicionado métodos de inserção no banco, com exemplos

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\dev-Takeshi\\Documents\\CursoEFCore.mdf;Integrated Security=True;Connect Timeout=30";
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CursoEFCore.Data;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -19,6 +22,78 @@ namespace CursoEFCore
             {
                 Console.WriteLine("Não existe migrações pendentes!");
             }
+
+            //InserirDados();
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 18m,
+                Tipo = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new ApplicationContext();
+            db.Produtos.Add(produto);
+
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"Foram afetados: {registros} registro(s)");
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 18m,
+                Tipo = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Maikon",
+                CEP = "99999000",
+                Cidade = "Mogi das Cruzes",
+                Estado = "SE",
+                Telefone = "99000001111",
+            };
+
+            var listaClientes = new[]
+            {
+                new Cliente
+                {
+                    Nome = "Maikon2",
+                    CEP = "99999000",
+                    Cidade = "Mogi das Cruzes",
+                    Estado = "SE",
+                    Telefone = "99000001111",
+                },
+                new Cliente
+                {
+                    Nome = "Maikon3",
+                    CEP = "99999000",
+                    Cidade = "Mogi das Cruzes",
+                    Estado = "SE",
+                    Telefone = "99000001111",
+                }
+        };
+
+            using var db = new ApplicationContext();
+            //db.AddRange(produto, cliente);
+
+            db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"Foram afetados: {registros} registro(s)");
         }
     }
 }
