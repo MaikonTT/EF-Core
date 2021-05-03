6289983e86d9bdf0776fd41b6779fccdbb2b788d
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-05-03 19:46:15 -0300

    Alterado limite da propriedade Nome da classe Cliente

--- Curso/Data/Configurations/ClienteConfiguration.cs
+++ Curso/Data/Configurations/ClienteConfiguration.cs
@@ -10,7 +10,7 @@ namespace CursoEFCore.Data.Configurations
         {
             builder.ToTable("Clientes");
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.Nome).HasColumnType("NVARCHAR(14)").IsRequired();
+            builder.Property(p => p.Nome).HasColumnType("NVARCHAR(100)").IsRequired();
             builder.Property(p => p.Telefone).HasColumnType("CHAR(11)");
             builder.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
             builder.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
---
+++ Curso/Migrations/20210503223108_AlterarTamanhoDeNome.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoEFCore.Migrations
+{
+    public partial class AlterarTamanhoDeNome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Clientes",
+                type: "NVARCHAR(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(14)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Clientes",
+                type: "NVARCHAR(14)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)");
+        }
+    }
+}
